a69135961f709f67a7ef5991e993311d515c43f2
Raihan Adam <hallwack.id@gmail.com>
2023-04-16 02:20:15 +0700

    feat: add hero image section

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import assets from "@/Data/assets";
 import MainLayout from "@/Layouts/MainLayout";
 import { Head } from "@inertiajs/react";
 import React from "react";
@@ -12,16 +13,16 @@ const Home = () => {
                 <div className="flex flex-col">
                     <section className="container mx-auto flex flex-col justify-center items-center py-8">
                         <div className="inline-flex items-center rounded-full bg-white px-2 py-3 gap-2 w-fit">
-                            <div className="w-80 text-center rounded-full bg-black text-white py-3">
+                            <div className="w-[360px] text-center rounded-full bg-black text-white py-3">
                                 Learn and Get Certificates
                             </div>
-                            <div className="w-80 text-center">
+                            <div className="w-[360px] text-center">
                                 Build Your Career
                             </div>
                         </div>
                     </section>
-                    <section>
-                        <img />
+                    <section className="flex justify-center">
+                        <img src={assets.imgHero} className="w-11/12" />
                     </section>
                 </div>
             </MainLayout>
