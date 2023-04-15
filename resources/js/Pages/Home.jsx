c40c28af63b24ad08414d9f49cb1becc0b0ceafc
Raihan Adam <hallwack.id@gmail.com>
2023-04-15 21:25:00 +0700

    feat: add button below navbar

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -9,7 +9,21 @@ const Home = () => {
                 <title>Home</title>
             </Head>
             <MainLayout>
-                <div>Home</div>
+                <div className="flex flex-col">
+                    <section className="container mx-auto flex flex-col justify-center items-center py-8">
+                        <div className="inline-flex items-center rounded-full bg-white px-2 py-3 gap-2 w-fit">
+                            <div className="w-80 text-center rounded-full bg-black text-white py-3">
+                                Learn and Get Certificates
+                            </div>
+                            <div className="w-80 text-center">
+                                Build Your Career
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        <img />
+                    </section>
+                </div>
             </MainLayout>
         </>
     );
