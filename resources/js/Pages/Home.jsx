fe161e0dfce970ee92a7c34275718d9f56cc04c0
Raihan Adam <hallwack.id@gmail.com>
2023-04-15 19:52:15 +0700

    feat: add Home page component

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/Layouts/MainLayout";
+import React from "react";
+
+const Home = () => {
+    return (
+        <MainLayout>
+            <div>Home</div>
+        </MainLayout>
+    );
+};
+
+export default Home;
