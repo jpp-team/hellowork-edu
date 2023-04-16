b83041b2d10e5a301ac0d7764beee536321af052
Raihan Adam <hallwack.id@gmail.com>
2023-04-17 05:50:38 +0700

    fix: change from swiperjs to react-splide in Home page component

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,10 +1,9 @@
-import assets from "@/Data/assets";
-import MainLayout from "@/Layouts/MainLayout";
-import { Head } from "@inertiajs/react";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import { Autoplay } from "swiper";
+import { Head } from "@inertiajs/react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import MainLayout from "@/Layouts/MainLayout";
+import assets from "@/Data/assets";
+import "@splidejs/react-splide/css";
 
 const Home = () => {
     return (
@@ -28,73 +27,65 @@ const Home = () => {
                         <img src={assets.imgHero} className="w-11/12" />
                     </section>
                     <section className="container mx-auto flex flex-col justify-center items-center py-8">
-                        <div className="flex min-w-full">
-                            <button>iyhh</button>
-                            <Swiper
-                                modules={[Autoplay]}
-                                autoplay={{
-                                    delay: 2000,
-                                    disableOnInteraction: true,
-                                }}
-                                loop={true}
-                                direction="horizontal"
-                                spaceBetween={60}
-                                slidesPerView={2}
-                                width={360}
-                            >
-                                <SwiperSlide>
-                                    <div className="flex flex-col justify-center items-center gap-2 w-min">
-                                        <div className="bg-white rounded-full w-32 h-32 flex justify-center items-center">
-                                            <img
-                                                src={assets.dibimbing}
-                                                className="object-center w-24 h-auto"
-                                            />
-                                        </div>
-                                        <p className="text-center">
-                                            Japanese Language
-                                        </p>
+                        <Splide
+                            options={{
+                                pagination: false,
+                                type: "loop",
+                                perPage: 3,
+                                gap: "1rem",
+                                rewind: true,
+                                width: 1205,
+                                autoplay: true,
+                                interval: 2000,
+                            }}
+                        >
+                            <SplideSlide>
+                                <div className="flex flex-col justify-center items-center gap-2">
+                                    <div className="bg-white rounded-full w-32 h-32 flex justify-center items-center">
+                                        <img
+                                            src={assets.dibimbing}
+                                            className="object-center w-24 h-auto"
+                                        />
                                     </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="flex flex-col justify-center items-center gap-2 w-min">
-                                        <div className="bg-white rounded-full w-32 h-32 flex justify-center items-center">
-                                            <img
-                                                src={assets.arkalearn}
-                                                className="object-center w-24 h-auto"
-                                            />
-                                        </div>
-                                        <p className="text-center">IT Skills</p>
+                                    <p className="text-center">
+                                        Japanese Language
+                                    </p>
+                                </div>
+                            </SplideSlide>
+                            <SplideSlide>
+                                <div className="flex flex-col justify-center items-center gap-2">
+                                    <div className="bg-white rounded-full w-32 h-32 flex justify-center items-center">
+                                        <img
+                                            src={assets.arkalearn}
+                                            className="object-center w-24 h-auto"
+                                        />
                                     </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="flex flex-col justify-center items-center gap-2 w-min">
-                                        <div className="bg-white rounded-full w-32 h-32 flex justify-center items-center">
-                                            <img
-                                                src={assets.comingSoon}
-                                                className="object-center w-24 h-auto"
-                                            />
-                                        </div>
-                                        <p className="text-center">
-                                            Coming Soon
-                                        </p>
+                                    <p className="text-center">IT Skills</p>
+                                </div>
+                            </SplideSlide>
+                            <SplideSlide>
+                                <div className="flex flex-col justify-center items-center gap-2">
+                                    <div className="bg-white rounded-full w-32 h-32 flex justify-center items-center">
+                                        <img
+                                            src={assets.comingSoon}
+                                            className="object-center w-24 h-auto"
+                                        />
                                     </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="flex flex-col justify-center items-center gap-2 w-min">
-                                        <div className="bg-white rounded-full w-32 h-32 flex justify-center items-center">
-                                            <img
-                                                src={assets.comingSoon}
-                                                className="object-center w-24 h-auto"
-                                            />
-                                        </div>
-                                        <p className="text-center">
-                                            Coming Soon
-                                        </p>
+                                    <p className="text-center">Coming Soon</p>
+                                </div>
+                            </SplideSlide>
+                            <SplideSlide>
+                                <div className="flex flex-col justify-center items-center gap-2">
+                                    <div className="bg-white rounded-full w-32 h-32 flex justify-center items-center">
+                                        <img
+                                            src={assets.comingSoon}
+                                            className="object-center w-24 h-auto"
+                                        />
                                     </div>
-                                </SwiperSlide>
-                            </Swiper>
-                            <button>affh</button>
-                        </div>
+                                    <p className="text-center">Coming Soon</p>
+                                </div>
+                            </SplideSlide>
+                        </Splide>
                     </section>
                     <section
                         className="border-[#DF5B6A] border-t-2 py-20 bg-contain bg-no-repeat bg-right-bottom"
@@ -118,7 +109,10 @@ const Home = () => {
                                     </div>
                                     <div className="flex flex-col w-48">
                                         <img src={assets.appStore} />
-                                        <img src={assets.playStore} className="w-max" />
+                                        <img
+                                            src={assets.playStore}
+                                            className="w-max"
+                                        />
                                     </div>
                                 </div>
                             </div>
