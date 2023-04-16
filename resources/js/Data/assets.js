8aa484a20f9a20f7ee235a196d1c795297bf23b2
Raihan Adam <hallwack.id@gmail.com>
2023-04-17 03:05:47 +0700

    feat: register download badge and about bg image

--- resources/js/Data/assets.js
+++ resources/js/Data/assets.js
@@ -3,7 +3,10 @@ import imgHero from "@/Assets/landing/hero-img.png";
 import dibimbing from "@/Assets/landing/dibimbing.png";
 import arkalearn from "@/Assets/landing/arka-learn.png";
 import comingSoon from "@/Assets/landing/coming-soon.png";
-import aboutImg from "@/Assets/landing/about-img.png"
+import aboutImg from "@/Assets/landing/about-img.png";
+import bgAbout from "@/Assets/landing/bg-about.png";
+import playStore from "@/Assets/landing/google-play-badge.png";
+import appStore from "@/Assets/landing/app-store-badge.svg";
 
 const assets = {
     logoMin,
@@ -11,7 +14,10 @@ const assets = {
     dibimbing,
     arkalearn,
     comingSoon,
-    aboutImg
+    aboutImg,
+    bgAbout,
+    playStore,
+    appStore,
 };
 
 export default assets;
