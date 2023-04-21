0ccb6b7aa31b969abb6223d1c6fd3e965b0ffef0
Raihan Adam <hallwack.id@gmail.com>
2023-04-21 23:03:42 +0700

    fix: change footer using FooterAbout component

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import FloatingButton from "@/Components/FloatingButton";
 import Footer from "@/Components/Footer";
 import Navbar from "@/Components/Navbar";
+import FooterAbout from "@/Components/FooterAbout";
 
 const MainLayout = ({ children }) => {
     return (
         <div className="flex flex-col min-h-screen antialiased bg-[#FFF8F0]">
             <Navbar />
             {children}
-            <Footer />
+            <FooterAbout />
             <FloatingButton />
         </div>
     );
