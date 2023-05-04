0bf2ef5b78cbd01cfd5bfce85350066d0d2c2951
Raihan Adam <hallwack.id@gmail.com>
2023-05-04 11:45:37 +0700

    fix: fixing classname and fixing menuHidden state

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -3,11 +3,11 @@ import React, { useState } from "react";
 import PrimaryButton from "./PrimaryButton";
 
 const menus = [
-    {name: ''}
+    { name: '' }
 ]
 
 const Navbar = () => {
-    const [menuIsHidden, setMenuIsHidden] = useState(true);
+    const [menuIsHidden, setMenuIsHidden] = useState(false);
 
     function handleMenuButtonClick() {
         setMenuIsHidden(!menuIsHidden);
@@ -29,16 +29,16 @@ const Navbar = () => {
                         </li>
                     </ul>
 
-                    <div class="md:hidden flex items-center">
-                        <button class="outline-none mobile-menu-button" onClick={handleMenuButtonClick}>
-                            <svg class=" w-8 h-12 text-gray-800 hover:text-gray-600 "
-                                x-show="!showMenu"
+                    <div className="md:hidden flex items-center">
+                        <button className="outline-none mobile-menu-button" onClick={handleMenuButtonClick}>
+                            <svg
                                 fill="none"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                viewBox="0 0 24 24"
                                 stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                className="w-8 h-12 text-gray-800 hover:text-gray-600"
+                                viewBox="0 0 24 24"
                             >
                                 <path d="M4 6h16M4 12h16M4 18h16"></path>
                             </svg>
@@ -46,12 +46,12 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            <div class="md:hidden mobile-menu">
+            <div className="md:hidden mobile-menu">
                 {menuIsHidden && (
-                    <ul class="">
-                        <li><a href="#" class="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">Course</a></li>
-                        <li><a href="#" class="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">Login</a></li>
-                        <li><a href="#" class="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">Register</a></li>
+                    <ul className="">
+                        <li><a href="#" className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">Course</a></li>
+                        <li><a href="#" className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">Login</a></li>
+                        <li><a href="#" className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">Register</a></li>
                     </ul>
                 )}
             </div>
