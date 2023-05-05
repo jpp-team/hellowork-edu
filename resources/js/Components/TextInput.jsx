08ab103a555e01c081969f11839f5ae301a7de4a
Raihan Adam <hallwack.id@gmail.com>
2023-05-05 21:23:21 +0700

    fix: fixing input type condition when type is file and textarea

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,6 +1,9 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from "react";
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
+export default forwardRef(function TextInput(
+    { type = "text", className = "", isFocused = false, ...props },
+    ref
+) {
     const input = ref ? ref : useRef();
 
     useEffect(() => {
@@ -9,15 +12,37 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
         }
     }, []);
 
+    if (type === "textarea") {
+        return (
+            <div className="flex flex-col items-start">
+                <textarea
+                    ref={ref}
+                    {...props}
+                    className={`border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ${className}`}
+                ></textarea>
+            </div>
+        );
+    }
+
+    if (type === "upload") {
+        return (
+            <div className="flex flex-col items-start">
+                <input
+                    type="file"
+                    className={`block w-full text-sm text-slate-50 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100 ${className}`}
+                    ref={ref}
+                    {...props}
+                />
+            </div>
+        );
+    }
+
     return (
         <div className="flex flex-col items-start">
             <input
                 {...props}
                 type={type}
-                className={
-                    'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
-                    className
-                }
+                className={`border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ${className}`}
                 ref={input}
             />
         </div>
