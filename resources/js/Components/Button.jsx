bde72f35563146661f1caf31b18d0eb39045d3bf
Raihan Adam <hallwack.id@gmail.com>
2023-05-05 21:16:48 +0700

    feat: add disabled in button component

--- resources/js/Components/Button.jsx
+++ resources/js/Components/Button.jsx
@@ -1,21 +1,26 @@
 import clsx from "clsx";
 import React from "react";
 
-const Button = ({ variant, className, children, ...props }) => {
+const Button = ({ variant, disable, className, children, ...props }) => {
     const variantClasses = {
-        primary: "bg-primary/80 text-white border border-transparent hover:bg-primary focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:outline-none",
-        secondary: "bg-secondary/90 text-white border border-transparent hover:bg-secondary focus:ring-2 focus:ring-secondary focus:ring-offset-2 focus:outline-none",
-        outline: "bg-primary/10 border border-primary hover:bg-primary/30 focus:ring-2 focus:ring-primary/80 focus:ring-offset-2 focus:outline-none",
+        primary:
+            "bg-primary/80 text-white border border-transparent hover:bg-primary focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:outline-none",
+        secondary:
+            "bg-secondary/90 text-white border border-transparent hover:bg-secondary focus:ring-2 focus:ring-secondary focus:ring-offset-2 focus:outline-none",
+        outline:
+            "bg-primary/10 border border-primary hover:bg-primary/30 focus:ring-2 focus:ring-primary/80 focus:ring-offset-2 focus:outline-none",
     };
 
     return (
         <button
             className={clsx(
-                "inline-flex items-center px-8 py-2 rounded-md transition ease-in-out duration-200",
+                `inline-flex items-center px-6 py-2 rounded-md transition ease-in-out duration-200`,
+                disable && "opacity-40",
                 variantClasses[variant],
                 className
             )}
             {...props}
+            disabled={disable}
         >
             {children}
         </button>
