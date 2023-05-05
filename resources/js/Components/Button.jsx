eaf7f8cd5439e6c6b0ce9a2a44cd4cdd5df6d33f
Raihan Adam <hallwack.id@gmail.com>
2023-05-05 21:23:29 +0700

    chore: change import position

--- resources/js/Components/Button.jsx
+++ resources/js/Components/Button.jsx
@@ -1,5 +1,5 @@
-import clsx from "clsx";
 import React from "react";
+import clsx from "clsx";
 
 const Button = ({ variant, disable, className, children, ...props }) => {
     const variantClasses = {
