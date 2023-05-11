a229b26509dbf7c30c9ee625e2b7a1a9e667d375
Raihan Adam <hallwack.id@gmail.com>
2023-05-11 07:32:11 +0700

    fix: fixing navbar button when in mobile & user login

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -3,6 +3,8 @@ import { router, usePage } from "@inertiajs/react";
 import React, { useState } from "react";
 import Button from "./Button";
 import { Link } from "@inertiajs/react";
+import Dropdown from "./Dropdown";
+import useWidthMobile from "@/Hooks/useWidthMobile";
 
 const menus = [{ name: "" }];
 
@@ -10,6 +12,7 @@ const Navbar = () => {
     const { props } = usePage();
     const { user } = props.auth;
     const [menuIsHidden, setMenuIsHidden] = useState(false);
+    const isMobile = useWidthMobile();
 
     function handleMenuButtonClick() {
         setMenuIsHidden(!menuIsHidden);
@@ -21,31 +24,7 @@ const Navbar = () => {
                 <div className="flex justify-between items-center p-8">
                     <img src={logoMin} />
                     <ul className="hidden md:flex items-center gap-2">
-                        {user ? (
-                            <>
-                                <li className="mr-4">
-                                    <p>Welcome, {user.first_name}</p>
-                                </li>
-                                <li>
-                                    <Link href="/dashboard">
-                                        <Button variant="secondary">
-                                            Dashboard
-                                        </Button>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        method="post"
-                                        as="button"
-                                        href="/logout"
-                                    >
-                                        <Button variant="secondary">
-                                            Logout
-                                        </Button>
-                                    </Link>
-                                </li>
-                            </>
-                        ) : (
+                        {!user ? (
                             <>
                                 <li>
                                     <Link href="/">
@@ -69,74 +48,128 @@ const Navbar = () => {
                                     </Link>
                                 </li>
                             </>
-                        )}
-                    </ul>
-
-                    <div className="md:hidden flex items-center">
-                        <button
-                            className="outline-none mobile-menu-button"
-                            onClick={handleMenuButtonClick}
-                        >
-                            <svg
-                                fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                className="w-8 h-12 text-gray-800 hover:text-gray-600"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M4 6h16M4 12h16M4 18h16"></path>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className="md:hidden mobile-menu">
-                {menuIsHidden && (
-                    <ul className="flex flex-col gap-2">
-                        {user ? (
+                        ) : (
                             <>
-                                <li>
+                                <li className="mr-4">
                                     <p>Welcome, {user.first_name}</p>
                                 </li>
                                 <li>
-                                    <Link href="/dashboard" className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">
+                                    <Link href="/dashboard">
+                                        <Button variant="secondary">
                                             Dashboard
+                                        </Button>
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link
-                                        method="post"
-                                        as="button"
-                                        href="/logout"
-                            className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300"
-                                    >
+                                    <Link method="post" href="/logout">
+                                        <Button variant="secondary">
                                             Logout
-                                    </Link>
-                                </li>
-                            </>
-                        ) : (
-                            <>
-                                <li>
-                                    <Link href="/" className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">
-                                            Course
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/login" className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">
-                                            Login
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/register" className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300">
-                                            Register
+                                        </Button>
                                     </Link>
                                 </li>
                             </>
                         )}
                     </ul>
-                )}
+                    {isMobile && (
+                        <>
+                            {!user ? (
+                                <div className="md:hidden flex items-center">
+                                    <button
+                                        className="outline-none mobile-menu-button"
+                                        onClick={handleMenuButtonClick}
+                                    >
+                                        <svg
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            className="w-8 h-12 text-gray-800 hover:text-gray-600"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path d="M4 6h16M4 12h16M4 18h16"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                            ) : (
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="flex rounded-md md:hidden">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                            >
+                                                {user.first_name}
+
+                                                <svg
+                                                    className="ml-2 -mr-0.5 h-4 w-4"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
+
+                                    <Dropdown.Content>
+                                        <Dropdown.Link
+                                            href={route("dashboard")}
+                                        >
+                                            Dashboard
+                                        </Dropdown.Link>
+                                        <Dropdown.Link
+                                            href={route("logout")}
+                                            method="post"
+                                            as="button"
+                                        >
+                                            Log Out
+                                        </Dropdown.Link>
+                                    </Dropdown.Content>
+                                </Dropdown>
+                            )}
+                        </>
+                    )}
+                </div>
+                <div className="md:hidden mobile-menu">
+                    {menuIsHidden && (
+                        <ul className="flex flex-col gap-2">
+                            {!user && (
+                                <>
+                                    <li>
+                                        <Link
+                                            href="/"
+                                            className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300"
+                                        >
+                                            Course
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            href="/login"
+                                            className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300"
+                                        >
+                                            Login
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            href="/register"
+                                            className="block text-sm px-2 py-4 hover:text-white hover:bg-gray-800 transition duration-300"
+                                        >
+                                            Register
+                                        </Link>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    )}
+                </div>
             </div>
         </nav>
     );
