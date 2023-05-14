e0414b79081b907f2f4f1fc96dd5023fc6284d9f
Raihan Adam <hallwack.id@gmail.com>
2023-05-14 21:02:59 +0700

    feat: add course routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
@@ -28,5 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('/course', CourseController::class);
 
 require __DIR__ . '/auth.php';
