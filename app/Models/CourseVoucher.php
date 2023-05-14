aa289f3b380e2600f44b4ad8746a31999e74734b
Raihan Adam <hallwack.id@gmail.com>
2023-05-14 13:29:27 +0700

    feat: create CourseVoucher model

---
+++ app/Models/CourseVoucher.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourseVoucher extends Model
+{
+    use HasFactory;
+
+    protected $table = 'course_voucher';
+    public $timestamps = false;
+}
