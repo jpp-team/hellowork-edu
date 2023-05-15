4c984b5fa176b65aafde18313da282ec34140e05
Raihan Adam <hallwack.id@gmail.com>
2023-05-15 20:21:40 +0700

    fix: add function to show toast when success or error in Toast component

--- resources/js/Components/Toast.jsx
+++ resources/js/Components/Toast.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { usePage } from "@inertiajs/react";
+import React, { useEffect } from "react";
+import { Toaster, toast } from "react-hot-toast";
 
 const Toast = () => {
-  return (
-    <div>Toast</div>
-  )
-}
+    const { flash, errors } = usePage().props;
 
-export default Toast
+    useEffect(() => {
+        if (errors.error) {
+            toast.error(errors.error, {
+                position: "bottom-right",
+                duration: 2000,
+            });
+        } else if (flash.success !== null) {
+            toast.success(flash.success, {
+                position: "bottom-right",
+                duration: 2000,
+            });
+        } else if (flash.error !== null) {
+            toast.error(flash.error, {
+                position: "bottom-right",
+                duration: 2000,
+            });
+        }
+        console.log()
+    }, [flash, errors]);
+
+    return <Toaster />;
+};
+
+export default Toast;
