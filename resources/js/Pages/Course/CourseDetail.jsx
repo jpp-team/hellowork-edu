4ccbf909788895d3aa9a51463f213b2bc71a396a
Raihan Adam <hallwack.id@gmail.com>
2023-05-21 22:00:37 +0700

    fix: change CourseDetail page component props, form, and data card

--- resources/js/Pages/Course/CourseDetail.jsx
+++ resources/js/Pages/Course/CourseDetail.jsx
@@ -1,12 +1,12 @@
+import React from "react";
 import Button from "@/Components/Button";
 import MainLayout from "@/Layouts/MainLayout";
 import { Head, useForm } from "@inertiajs/react";
-import React from "react";
 
-const CourseDetail = ({ course, courseVoucher }) => {
+const CourseDetail = ({ course, courseVoucher, courseId, courseAll }) => {
     const { data, post, processing } = useForm({
-        voucher: courseVoucher.id,
-        course: course.id,
+        voucher: 0,
+        course: courseId,
     });
 
     const submit = (event) => {
@@ -19,59 +19,129 @@ const CourseDetail = ({ course, courseVoucher }) => {
     return (
         <MainLayout>
             <Head>
-                <title>{course.name}</title>
+                <title>{course?.name ?? "Daftar Course"}</title>
             </Head>
             <div className="w-full bg-primary h-80 flex flex-col gap-4 justify-center items-center pt-24">
                 <h1 className="text-black text-4xl font-semibold">
-                    {course.name}
+                    {course?.name ?? "Daftar Course"}
                 </h1>
                 <span className="text-secondary">Home - Course</span>
             </div>
-            <div className="container mx-auto flex gap-2">
-                <form
-                    className="max-w-sm rounded-lg overflow-hidden shadow-md my-12"
-                    onSubmit={submit}
-                >
-                    <img
-                        src={
-                            import.meta.env.VITE_URL_COURSE_ASSETS +
-                            course.image
-                        }
-                        className="object-cover w-full"
-                    />
-                    <div className="py-4 flex flex-col gap-4">
-                        <div className="px-6 gap-2 flex flex-col">
-                            <h2 className="text-2xl font-medium">
-                                {course.name}
-                            </h2>
-                            <span className="inline-block bg-slate-200 rounded-full px-3 py-1 font-semibold text-secondary text-sm w-fit">
-                                {course.course_category.name}
-                            </span>
-                        </div>
-                        <div className="px-6 mb-2">
-                            <input
-                                type="hidden"
-                                value={data.voucher}
-                                name="voucher"
-                                id="voucher"
+            <div className="container mx-auto flex gap-4">
+                {course ? (
+                    <>
+                        {courseVoucher ? (
+                            courseVoucher.map((voucher, index) => (
+                                <form
+                                    className="max-w-sm rounded-lg overflow-hidden shadow-md my-12"
+                                    onSubmit={submit}
+                                    key={index}
+                                >
+                                    <img
+                                        src={
+                                            import.meta.env
+                                                .VITE_URL_COURSE_ASSETS +
+                                            course.image
+                                        }
+                                        className="object-cover w-full"
+                                    />
+                                    <div className="py-4 flex flex-col gap-4">
+                                        <div className="px-6 gap-2 flex flex-col">
+                                            <h2 className="text-2xl font-medium">
+                                                {course.name}
+                                            </h2>
+                                            <span className="inline-block bg-slate-200 rounded-full px-3 py-1 font-semibold text-secondary text-sm w-fit">
+                                                {course.course_category.name}
+                                            </span>
+                                        </div>
+                                        <div className="px-6 mb-2">
+                                            <input
+                                                type="hidden"
+                                                value={voucher.id}
+                                                name="voucher"
+                                                id="voucher"
+                                            />
+                                            <input
+                                                type="hidden"
+                                                value={data.course}
+                                                name="course"
+                                                id="course"
+                                            />
+                                            <Button
+                                                className="w-full justify-center"
+                                                variant="primary"
+                                                type="submit"
+                                                disable={processing}
+                                            >
+                                                Redeem
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </form>
+                            ))
+                        ) : (
+                            <div className="my-12 w-full">
+                                <p className="text-2xl font-semibold text-center">
+                                    Voucher Course Tidak Ada
+                                </p>
+                            </div>
+                        )}
+                    </>
+                ) : courseAll ? (
+                    courseAll.map((voucher, index) => (
+                        <form
+                            className="max-w-sm rounded-lg overflow-hidden shadow-md my-12"
+                            onSubmit={submit}
+                            key={index}
+                        >
+                            <img
+                                src={
+                                    import.meta.env.VITE_URL_COURSE_ASSETS +
+                                    voucher.course.image
+                                }
+                                className="object-cover w-full"
                             />
-                            <input
-                                type="hidden"
-                                value={data.course}
-                                name="course"
-                                id="course"
-                            />
-                            <Button
-                                className="w-full justify-center"
-                                variant="primary"
-                                type="submit"
-                                disable={processing}
-                            >
-                                Redeem
-                            </Button>
-                        </div>
+                            <div className="py-4 flex flex-col gap-4">
+                                <div className="px-6 gap-2 flex flex-col">
+                                    <h2 className="text-2xl font-medium">
+                                        {voucher.course.name}
+                                    </h2>
+                                    <span className="inline-block bg-slate-200 rounded-full px-3 py-1 font-semibold text-secondary text-sm w-fit">
+                                        {voucher.course.course_category.name}
+                                    </span>
+                                </div>
+                                <div className="px-6 mb-2">
+                                    <input
+                                        type="hidden"
+                                        value={voucher.id}
+                                        name="voucher"
+                                        id="voucher"
+                                    />
+                                    <input
+                                        type="hidden"
+                                        value={voucher.course.id}
+                                        name="course"
+                                        id="course"
+                                    />
+                                    <Button
+                                        className="w-full justify-center"
+                                        variant="primary"
+                                        type="submit"
+                                        disable={processing}
+                                    >
+                                        Redeem
+                                    </Button>
+                                </div>
+                            </div>
+                        </form>
+                    ))
+                ) : (
+                    <div className="my-12 w-full">
+                        <p className="text-2xl font-semibold text-center">
+                            Course Tidak Ada
+                        </p>
                     </div>
-                </form>
+                )}
             </div>
         </MainLayout>
     );
