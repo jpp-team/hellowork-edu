2c2de30ea801ca6bdb6040ad0c85fdaa17d98d7f
Raihan Adam <hallwack.id@gmail.com>
2023-05-22 03:21:24 +0700

    fix: delete profile link in AuthenticatedLayout layout component

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -55,7 +55,6 @@ export default function Authenticated({ auth, header, children }) {
                                     </Dropdown.Trigger>
 
                                     <Dropdown.Content>
-                                        <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
                                         <Dropdown.Link href={route('logout')} method="post" as="button">
                                             Log Out
                                         </Dropdown.Link>
